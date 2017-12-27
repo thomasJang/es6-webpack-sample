944b50b60ee69159524a1b64189ad29042457502
Thomas Jang <tom@axisj.com>
2017-12-28 00:33:19 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let test = '';
+
+function a() {
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const basePath = __dirname
+
+module.exports = {
+  context: path.join(basePath, '.'),
+  resolve: {
+    extensions: ['.js']
+  },
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.join(basePath, './dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 4000
+  }
+}
